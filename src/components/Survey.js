0ad841b3efee3dd5191230ea3ac2dd8de87aca33
hikurangi <hikurangi@gmail.com>
2017-11-08 10:11:22 +1300

    fix Survey component syntax

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
 
 class Survey extents Component {
-  <p>
-    SURVEY HERE
-  </p>
+  render() {
+    return (
+      <p>
+        SURVEY HERE
+      </p>
+    )
+  }
 }
 
 export default Survey
