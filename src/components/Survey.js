d26ab26a9fcb7fab1bba16e4c28c66c081c441f6
hikurangi <hikurangi@gmail.com>
2017-11-08 11:34:42 +1300

    add react key to list items

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -3,7 +3,11 @@ import data from '../data/data'
 
 class Survey extends Component {
   render() {
-    const questions = data.map(item => <li>{item}</li>)
+    const questions = data.map((item, index) => (
+      <li key={index}>
+        {item}
+      </li>
+    ))
     return (
       <ol>
         {questions}
