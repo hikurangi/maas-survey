8a8bcbbc06b313e51f269d91a8b2578b16b761f1
hikurangi <hikurangi@gmail.com>
2017-11-08 14:00:24 +1300

    give each list item an accessible id

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -2,9 +2,17 @@ import React, {Component} from 'react'
 import data from '../data/data'
 
 class Survey extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
   render() {
     const questions = data.map((item, index) => (
-      <li key={index}>
+      <li
+        key={index}
+        id={index}
+      >
         {item}
       </li>
     ))
