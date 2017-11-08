98d09e58a48d8b9bf7be3a6489639407ef7b2de4
hikurangi <hikurangi@gmail.com>
2017-11-09 12:02:47 +1300

    remove log callback from setState call, move scale array up to this component, add mean calculation

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -4,19 +4,25 @@ import data from '../data/data'
 import Question from './Question'
 
 class Survey extends Component {
+
   constructor(props) {
     super(props);
     this.state = {}
   }
 
   handleChange = e => {
-    this.setState({[e.target.name]:+e.target.value}, console.log({state: this.state}))
+    this.setState({[e.target.name] :+ e.target.value})
   }
 
   render() {
+    const scale = [1, 2, 3, 4, 5, 6]
+    const mean = Object.keys(this.state).length === 15
+      ? Object.values(this.state).reduce((a, b) => a + b) / 15
+      : undefined
+    console.log({mean});
     const questions = data.map((item, index) => (
       <MuiThemeProvider key={index}>
-        <Question id={index} handleChange={this.handleChange} item={item}/>
+        <Question id={index} handleChange={this.handleChange} item={item} scale={scale}/>
       </MuiThemeProvider>
     ))
     return (
