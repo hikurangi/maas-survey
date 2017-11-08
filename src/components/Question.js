dc044362855716f9f85f983a187a53a6c2cea804
hikurangi <hikurangi@gmail.com>
2017-11-08 14:46:40 +1300

    produce radio buttons iteratively using map

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
 
-const Question = props => (
-    <li id={props.id}>
+const Question = props => {
+  const buttons = [1, 2, 3, 4, 5, 6].map((item, index) => (
+    <RadioButton
+      value={index}
+      label={index}
+      onChange={props.handleChange}
+    />
+  ))
+  return (
+    <li key={props.id} id={props.id}>
       {props.item}
-      <RadioButtonGroup>
-        <RadioButton></RadioButton>
+      <RadioButtonGroup name="scale">
+        {buttons}
       </RadioButtonGroup>
     </li>
   )
+}
 
 export default Question
