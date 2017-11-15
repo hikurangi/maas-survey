da789d951ab0353837851833236220c683a46bbc
hikurangi <hikurangi@gmail.com>
2017-11-15 15:42:51 +1300

    display 2 d.p. only if mean is not effectively an integer

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ const Result = props => {
   const mean = props.values.reduce((a, b) => a + b, 0) / props.values.length
   if (props.values.length === props.dataLength) {
     return (
-      <h1>Your MAAS Score is {mean.toFixed(2)}</h1>
+      <h1>Your MAAS Score is {Math.floor(mean) === mean ? mean : mean.toFixed(2)}</h1>
     )
   } else {
     return null
