7b32ee6e93931ec0c42cb88f306fdb31de720424
hikurangi <hikurangi@gmail.com>
2017-11-22 22:35:21 +1300

    fix formatting

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton'
+import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton'
 
 const style = {
   questionStyle: {
